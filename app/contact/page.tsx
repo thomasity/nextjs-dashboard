0718376481f3ea41ad85371131f9320595327521
thomasity <callent@umich.edu>
2025-08-07 17:56:26 -0400

    contact: "contact me"

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -7,6 +7,7 @@ export default function Page() {
   return (
     <main className={styles['page-wrapper']}>
         <section className={styles['section-wrapper']}>
+          <h2 className="m-4">Contact Me</h2>
           <ContactForm />
         </section>
     </main>
