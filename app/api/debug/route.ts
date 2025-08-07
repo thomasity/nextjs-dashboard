53534c91f01a22dc4acaa3cee6718e8a0e31c464
thomasity <callent@umich.edu>
2025-08-07 16:34:41 -0400

    removed env debug

--- app/api/debug/route.ts
+++
@@ -1,8 +0,0 @@
-// app/api/debug-env/route.ts
-import { NextResponse } from 'next/server';
-
-export async function GET() {
-  return NextResponse.json({
-    EMAIL_USERNAME: process.env.EMAIL_USERNAME || 'undefined',
-  });
-}
