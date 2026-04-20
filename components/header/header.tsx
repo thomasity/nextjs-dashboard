882df4e624b0bb5dd4b49d18718dde97a0a040a2
thomasity <callent@umich.edu>
2026-04-19 21:33:40 -0400

    Update header to stick to top of screen

--- components/header/header.tsx
+++ components/header/header.tsx
@@ -1,8 +1,5 @@
 'use client';
 import Link from 'next/link';
-import Image from 'next/image';
-import { useTheme } from 'next-themes';
-import React from 'react';
 import { usePathname } from 'next/navigation';
 import MobileHeader from './mobileHeader/mobileHeader';
 import useIsMobile from '@/lib/hooks/useIsMobile';
@@ -22,30 +19,34 @@ export default function Header() {
     const isMobile = useIsMobile();
 
     return (
-        <header>
+        <header className="sticky top-0 z-50 bg-(--bg-color) border-b border-(--border-color)">
             {isMobile ? (
                 <MobileHeader />
             ) : (
-            <nav className="w-full flex flex-row justify-between items-center">
-                <Link key={"root"} href="/"><ThemedImage lightSrc='/logo.png' darkSrc='/logo_dark.png' defaultSrc='/logo.png' alt='Logo' height={128} width={128} /></Link>
-                <div className="trapezoid-nav">
-                    {links.map((link) => {
-                        const path = link === 'Home' ? '/' : `/${link.toLowerCase()}`;
-                        const isActive = path === '/' ? pathname === path : pathname.startsWith(path);
-                        return (
-                            <Link
-                            key={link}
-                            href={path}
-                            className={clsx("trapezoid", isActive ? "text-[var(--font-inverse)] bg-[var(--dot-color)]" : "cursor-pointer hover:underline")}
-                            >
-                                {link}
-                            </Link>
-                        );
-                    })}
+            <nav className="w-full h-14 grid grid-cols-3 items-center">
+                <div className="flex items-center justify-start">
+                    <Link key={"root"} href="/"><ThemedImage lightSrc='/logo.png' darkSrc='/logo_dark.png' defaultSrc='/logo.png' alt='Logo' height={56} width={120} /></Link>
                 </div>
-                <div>
-                    <Link 
-                        href='https://github.com/thomasity' 
+                <div className="flex items-center justify-center">
+                    <div className="trapezoid-nav">
+                        {links.map((link) => {
+                            const path = link === 'Home' ? '/' : `/${link.toLowerCase()}`;
+                            const isActive = path === '/' ? pathname === path : pathname.startsWith(path);
+                            return (
+                                <Link
+                                    key={link}
+                                    href={path}
+                                    className={clsx("trapezoid", isActive ? "text-(--font-inverse) bg-(--dot-color)" : "cursor-pointer hover:underline")}
+                                >
+                                    {link}
+                                </Link>
+                            );
+                        })}
+                    </div>
+                </div>
+                <div className="flex items-center justify-end">
+                    <Link
+                        href='https://github.com/thomasity'
                         rel='noopener noreferrer'
                         target='_blank'
                         className="hover:underline nav-link"
@@ -53,12 +54,12 @@ export default function Header() {
                         github
                     </Link>
                     /
-                    <Link 
-                        href='https://www.linkedin.com/in/thomas-callen-410a11252' 
+                    <Link
+                        href='https://www.linkedin.com/in/thomas-callen-410a11252'
                         rel='noopener noreferrer'
                         target='_blank'
                         className="hover:underline nav-link mr-2"
-                        >
+                    >
                         linkedin
                     </Link>
                     <ThemeToggle />
